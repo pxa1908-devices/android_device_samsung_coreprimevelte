5dd19bb37a62751a722dd23eba49b390fea2f32f
notnoelchannel2 <notnoelchannel1@protonmail.com>
2026-07-27 23:11:13 +0200

    Update coreprimevelte.mk

--- coreprimevelte.mk
+++ coreprimevelte.mk
@@ -59,10 +59,6 @@ ifneq ($(TARGET_BUILD_VARIANT),user)
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0 \
     ro.adb.secure=0
-
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.secure=0 \
-    ro.adb.secure=0
 endif
 
 # Inherit from 
